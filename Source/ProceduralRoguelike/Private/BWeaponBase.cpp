39366e70ecf68a22a5c84d4706c709613ce25144
Buğra Akca <bgrakca@gmail.com>
2025-06-20 21:58:44 +0300

    Add weapon base class

---
+++ Source/ProceduralRoguelike/Private/BWeaponBase.cpp
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BWeaponBase.h"
+
+#include "NiagaraFunctionLibrary.h"
+#include "Components/AudioComponent.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+// Sets default values
+ABWeaponBase::ABWeaponBase()
+{
+    BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
+    RootComponent = BoxComp;
+
+    StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+    StaticMeshComp->SetupAttachment(RootComponent);
+
+    AudioComp = CreateDefaultSubobject<UAudioComponent>(TEXT("AudioComp"));
+    AudioComp->SetupAttachment(RootComponent);
+}
+
+void ABWeaponBase::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    DamageVfx();
+}
+
+void ABWeaponBase::BeginPlay()
+{
+    Super::BeginPlay();
+
+    BoxComp->OnComponentBeginOverlap.AddDynamic(this, &ABWeaponBase::OnActorOverlap);
+}
+
+UBoxComponent* ABWeaponBase::GetBoxComponent() const
+{
+    return BoxComp;
+}
+
+void ABWeaponBase::DamageVfx_Implementation()
+{
+    if (ensure(IsValid(this)))
+    {
+        UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactVfx, GetActorLocation(), GetActorRotation(), FVector(1), true,true);
+
+        UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation());
+
+        UGameplayStatics::PlayWorldCameraShake(this, ImpactShake, GetActorLocation(), ImpactShakeInnerRadius, ImpactShakeOuterRadius);
+    }
+}
+
+
+
---
+++ Source/ProceduralRoguelike/Public/BWeaponBase.h
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BWeaponBase.generated.h"
+
+class UNiagaraSystem;
+class UBoxComponent;
+
+UCLASS()
+class PROCEDURALROGUELIKE_API ABWeaponBase : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this actor's properties
+    ABWeaponBase();
+
+protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    TObjectPtr<UStaticMeshComponent> StaticMeshComp;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    TObjectPtr<UBoxComponent> BoxComp;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    TObjectPtr<UAudioComponent> AudioComp;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Effects")
+    TObjectPtr<UNiagaraSystem> ImpactVfx;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Effects")
+    TObjectPtr<USoundBase> ImpactSound;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Effects|Shake")
+    TSubclassOf<UCameraShakeBase> ImpactShake;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Effects|Shake")
+    float ImpactShakeInnerRadius;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Effects|Shake")
+    float ImpactShakeOuterRadius;
+    
+
+    UFUNCTION()
+    virtual void OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    void DamageVfx();
+
+public:
+    virtual void BeginPlay() override;
+
+    UFUNCTION(BlueprintCallable, Category = "Weapon")
+    UBoxComponent* GetBoxComponent() const;
+};
