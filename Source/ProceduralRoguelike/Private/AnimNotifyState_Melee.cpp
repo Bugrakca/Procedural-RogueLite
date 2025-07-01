910fe65ee702bebaaeba308f45df670635cb0713
Buğra Akca <bgrakca@gmail.com>
2025-07-01 22:57:42 +0300

    Add anim notify state weapon trace logic

---
+++ Source/ProceduralRoguelike/Private/AnimNotifyState_Melee.cpp
@@ -0,0 +1,82 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotifyState_Melee.h"
+
+#include "BCharacter.h"
+#include "BWeaponBase.h"
+#include "KismetTraceUtils.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Engine/EngineTypes.h"
+
+UAnimNotifyState_Melee::UAnimNotifyState_Melee()
+{
+    TraceChannel = ECC_Pawn;
+}
+
+void UAnimNotifyState_Melee::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+    // Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+    AActor* MyOwner = MeshComp->GetOwner();
+    if (ABCharacter* MyCharacter = Cast<ABCharacter>(MyOwner))
+    {
+        CurrentWeapon = MyCharacter->GetCurrentWeapon();
+    }
+
+    if (CurrentWeapon)
+    {
+        CurrentWeapon->ResetHitActors();
+        if (UStaticMeshComponent* WeaponMesh = CurrentWeapon->GetStaticMesh())
+        {
+            PreviousTopTrace = WeaponMesh->GetSocketLocation(TopSocketName);
+            PreviousBottomTrace = WeaponMesh->GetSocketLocation(BottomSocketName);
+        }
+    }
+    UE_LOG(LogTemp, Warning, TEXT("Weapon trace started, weapon: %s"), CurrentWeapon ? *CurrentWeapon->GetName() : TEXT("None"));
+}
+
+void UAnimNotifyState_Melee::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+    // Super::NotifyEnd(MeshComp, Animation, EventReference);
+    UE_LOG(LogTemp, Warning, TEXT("Weapon trace ended"));
+
+    // CurrentWeapon = nullptr;
+}
+
+void UAnimNotifyState_Melee::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+    if (!CurrentWeapon)
+        return;
+
+    UStaticMeshComponent* WeaponMesh = CurrentWeapon->GetStaticMesh();
+    if (!IsValid(WeaponMesh))
+        return;
+    
+    FVector CurrentTopTrace = WeaponMesh->GetSocketLocation("TopTrace");
+    FVector CurrentBottomTrace = WeaponMesh->GetSocketLocation("BottomTrace");
+    
+    float Distance = FVector::Dist(CurrentTopTrace, CurrentBottomTrace);
+    FRotator SocketRotation = FRotationMatrix::MakeFromX(CurrentBottomTrace - CurrentTopTrace).Rotator();
+    FVector HalfSize = FVector(Distance / 2.0f, 10.0f, 10.0f);
+
+    FCollisionShape CollisionShape = FCollisionShape::MakeBox(HalfSize);
+    
+    FCollisionQueryParams QueryParams;
+    QueryParams.AddIgnoredActor(MeshComp->GetOwner());
+    
+    QueryParams.bTraceComplex = false;
+
+    FHitResult Hit;
+    bool bHit = MeshComp->GetWorld()->SweepSingleByChannel(Hit, PreviousTopTrace, CurrentTopTrace, SocketRotation.Quaternion(), TraceChannel, CollisionShape, QueryParams);
+
+    DrawDebugBoxTraceSingle(MeshComp->GetWorld(), PreviousTopTrace, CurrentTopTrace, HalfSize, SocketRotation, EDrawDebugTrace::ForDuration, bHit, Hit, FLinearColor::Red, FLinearColor::Green, 1.0f);
+    
+    PreviousTopTrace = CurrentTopTrace;
+    PreviousBottomTrace = CurrentBottomTrace;
+    
+    if (bHit)
+    {
+        CurrentWeapon->TryApplyDamage(Hit);
+    }
+}
---
+++ Source/ProceduralRoguelike/Public/AnimNotifyState_Melee.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "AnimNotifyState_Melee.generated.h"
+
+class ABWeaponBase;
+enum ECollisionChannel : int;
+/**
+ * 
+ */
+UCLASS()
+class PROCEDURALROGUELIKE_API UAnimNotifyState_Melee : public UAnimNotifyState
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(EditAnywhere)
+    TEnumAsByte<ECollisionChannel> TraceChannel;
+
+    UPROPERTY(EditAnywhere)
+    FName TopSocketName;
+
+    UPROPERTY(EditAnywhere)
+    FName BottomSocketName;
+
+    UPROPERTY()
+    TObjectPtr<ABWeaponBase> CurrentWeapon;
+
+    FVector PreviousTopTrace;
+    
+    FVector PreviousBottomTrace;
+
+public:
+    virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
+
+    virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+    virtual void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference) override;
+
+    UAnimNotifyState_Melee();
+};
